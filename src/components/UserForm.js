fd426671b172ac41e948fd48bc13c383d012781d
malgorzata bozykowska <malgorzata.bozykowska@gmail.com>
2023-09-25 10:58:56 +0200

    Styled UserForm and Card

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,15 +1,17 @@
 import React from "react";
+import Card from "./UI/Card";
+import styles from "./UserForm.module.css";
 
 const UserForm = () => {
   return (
-    <div>
-      <form>
+    <Card>
+      <form className={styles["user-form"]}>
         <label>User name</label>
         <input type="text" />
         <label>Age (years)</label>
         <input type="number" />
       </form>
-    </div>
+    </Card>
   );
 };
 
