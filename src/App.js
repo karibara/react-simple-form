f624380f3a9db2eb257545fb9845b55eb75c2fa9
malgorzata bozykowska <malgorzata.bozykowska@gmail.com>
2023-10-03 10:04:24 +0200

    Add user data validation and invalid data message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import UserForm from "./components/UserForm";
 import UserList from "./components/UserList";
+import InvalidInputAlert from "./components/InvalidInputAlert";
 
 function App() {
   // use useState to create an array of users
   const [users, setUsers] = useState([]);
+  const [alert, setAlert] = useState();
+  const invalidData = "Please, enter a valid name and age (non-empty values)";
+  const invalidAge = "Please, enter a valid age ( > 0)";
 
   const userDataHandler = (newUser) => {
     // newUser is an object with entered data from form + added id
@@ -13,10 +17,18 @@ function App() {
       id: Math.random().toString(),
     };
 
-    // with useState new users are pushed to an array, and it is automaticly updated
-    setUsers((prevUsers) => {
-      return [newUser, ...prevUsers];
-    });
+    // validation for user name length and user age
+    if (userData.userAge < 1 || userData.userAge > 105) {
+      setAlert(<InvalidInputAlert message={invalidAge} />);
+    } else if (userData.userName.trim().length < 2) {
+      // console.log("user name is incorrect!!!");
+      setAlert(<InvalidInputAlert message={invalidData} />);
+    } else {
+      // with useState new users are pushed to an array, and it is automaticly updated
+      setUsers((prevUsers) => {
+        return [newUser, ...prevUsers];
+      });
+    }
   };
 
   // just check if passing data from form works
@@ -26,7 +38,8 @@ function App() {
     <div>
       <UserForm submitUserData={userDataHandler} />
       {/* array of users is passed to UserListr via props */}
-      <UserList addedUsers={users} />
+      {alert}
+      {/* <UserList addedUsers={users} /> */}
     </div>
   );
 }
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -3,19 +3,19 @@ import Card from "./UI/Card";
 import styles from "./UserList.module.css";
 
 const UserList = (props) => {
-  let usersNumber = props.addedUsers.length;
-  // console.log(usersNumber);
+  // let usersNumber = props.addedUsers.length;
+  // // console.log(usersNumber);
 
-  let message = "no users on the list";
+  // let message = "no users on the list";
 
-  if (usersNumber > 0) {
-    message = "list is working!!!!";
-  }
+  // if (usersNumber > 0) {
+  //   message = "list is working!!!!";
+  // }
 
   return (
     <Card>
       <div className={styles["user-frame"]}>
-        <p>{message}</p>
+        <p>message</p>
       </div>
     </Card>
   );
