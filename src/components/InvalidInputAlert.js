0adad499063204c85cdb8aae1cf2613659cdd499
Malgorzata Bozykowska <malgorzata.bozykowska@gmail.com>
2023-10-04 20:26:11 +0200

    Work on hiding alert

--- src/components/InvalidInputAlert.js
+++ src/components/InvalidInputAlert.js
@@ -4,39 +4,36 @@ import Button from "./UI/Button";
 
 import style from "./InvalidInputAlert.module.css";
 
+// lekcja 89 i 3/4
+// https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/quiz/5775802#overview
+
 const InvalidInputAlert = (props) => {
   const [alertOff, setAlertOff] = useState(false);
+
   const hideAlertHandler = () => {
-    setAlertOff(!alertOff);
+    setAlertOff(true);
   };
 
-  console.log(alertOff);
 
   return (
     <Card
       className={
-        !alertOff ? `${style["alert-card"]}` : `${!alertOff && style.hide}`
+        !alertOff ? `${style["alert-card"]}` : `${style["hide"]}`
       }
     >
       <div
-        className={
-          !alertOff ? `${style["alert-title"]}` : `${!alertOff && style.hide}`
-        }
+        className={style["alert-title"]}
       >
-        <h3 className={`${alertOff && style.hide}`}>Invalid input</h3>
+        <h3>Invalid input</h3>
       </div>
       <div className={style["alert-text"]}>
-        <h4 className={`${alertOff && style.hide}`}>{props.message}</h4>
+        <h4>{props.message}</h4>
         <div
-          className={
-            !alertOff
-              ? `${style["alert-div__btn"]}`
-              : `${!alertOff && style.hide}`
-          }
+          className={style["alert-div__btn"]}
         >
           <Button
             onClick={hideAlertHandler}
-            className={`${alertOff && style.hide}`}
+    
           >
             Okay
           </Button>
@@ -48,27 +45,4 @@ const InvalidInputAlert = (props) => {
 
 export default InvalidInputAlert;
 
-//
 
-// let alert;
-
-// // set state - alert is on and it is visible
-// const [alertOn, setAlertOn] = useState(true);
-
-// // handler, that switch alert of by clicking "okay" button
-// const hideAlertHandler = () => {
-//   setAlertOn(false);
-// };
-
-// // contidional which shows alert or switch it off
-// if (alertOn) {
-//   alert = (
-//     <Card>
-//       <h3>Invalid input</h3>
-//       {props.alertMessage}
-//       <Button onClick={hideAlertHandler}>Okay</Button>
-//     </Card>
-//   );
-// }
-
-// return { alert };
