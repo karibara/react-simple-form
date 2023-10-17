532ed1a5f578ab83d4a1a4f2b1fdd5e8c8857fc0
malgorzata bozykowska <malgorzata.bozykowska@gmail.com>
2023-10-17 08:56:40 +0200

    Change useState to Refs in UserForm

--- src/components/InvalidInputAlert.js
+++ src/components/InvalidInputAlert.js
@@ -1,4 +1,4 @@
-import React  from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import Card from "./UI/Card";
 import Button from "./UI/Button";
@@ -11,32 +11,30 @@ import style from "./InvalidInputAlert.module.css";
 const PortalAlert = (props) => {
   return (
     <div className={style["alert-container"]}>
-  <Card className={style["alert-card"]}>
-    <div className={style["alert-title"]}>
-      <h3>Invalid input</h3>
+      <Card className={style["alert-card"]}>
+        <div className={style["alert-title"]}>
+          <h3>Invalid input</h3>
+        </div>
+        <div className={style["alert-text"]}>
+          <h4>{props.message}</h4>
+          <div className={style["alert-div__btn"]}>
+            <Button onClick={props.onBtnClick}>Okay</Button>
+          </div>
+        </div>
+      </Card>
     </div>
-    <div className={style["alert-text"]}>
-      <h4>{props.message}</h4>
-      <div className={style["alert-div__btn"]}>
-        <Button onClick={props.onBtnClick}>Okay</Button>
-      </div>
-    </div>
-  </Card>
-</div>
-    )
-}
+  );
+};
 
 const InvalidInputAlert = (props) => {
-  // const [alertOff, setAlertOff] = useState(false);
-
-  console.log(props.onBtnClick);
-
   return (
     <>
-   {ReactDOM.createPortal(<PortalAlert onBtnClick={props.onBtnClick} message={props.message}/>, document.getElementById("alert-root"))}
+      {ReactDOM.createPortal(
+        <PortalAlert onBtnClick={props.onBtnClick} message={props.message} />,
+        document.getElementById("alert-root")
+      )}
     </>
   );
 };
 
 export default InvalidInputAlert;
-// className={!alertOff ? `${style["alert-card"]}` : `${style["hide"]}`}
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,22 +1,17 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import Card from "./UI/Card";
 import Button from "./UI/Button";
 import styles from "./UserForm.module.css";
 
 const UserForm = (props) => {
-  const [enteredName, setEnteredName] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
-
-  const nameHandler = (event) => {
-    setEnteredName(event.target.value);
-  };
-
-  const ageHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
   const submitHandler = (event) => {
     event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
 
     // object to store data from form
     const userData = {
@@ -26,19 +21,22 @@ const UserForm = (props) => {
 
     //the object with stored data from form (savingsFormData) is a function parameter, so we can pass it up to the parrent component by calling onSavesavingsFormData fnc in parent component
     props.submitUserData(userData);
+    // clean input with REFs (it is kind a DOM manipulation, and it is not a good practise)
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
 
     // clear inputs using two-way binding by adding the value property to input and set it to empty string
-    setEnteredName("");
-    setEnteredAge("");
+    // setEnteredName("");
+    // setEnteredAge("");
   };
 
   return (
     <Card className={styles["user-form__card"]}>
       <form className={styles["user-form"]} onSubmit={submitHandler}>
         <label>User name</label>
-        <input type="text" value={enteredName} onChange={nameHandler} />
+        <input type="text" ref={nameInputRef} />
         <label>Age (years)</label>
-        <input type="number" value={enteredAge} onChange={ageHandler} />
+        <input type="number" ref={ageInputRef} />
         <div className={styles["user-form__div"]}>
           <Button type="submit" className={styles["user-form__btn"]}>
             Add User
